47a6ea56af1b32e4dc983bc576b49a4037aeeb38
RobisonIanke <96997459+RobisonIanke@users.noreply.github.com>
2022-07-21 15:31:40 -0300

    Add files via upload

---
+++ 2 - JavaScript a Linguagem da Web/1 JavaScript primeiros passos com a linguagem/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,8 @@
+console.log("Trabalhando com atribuição de variavéis");
+
+const idade = 31;
+const nome = "Robison";
+const sobrenome = "Ianke";
+
+console.log(nome, sobrenome);
+console.log(`Meu nome é: ${nome} ${sobrenome}`);
